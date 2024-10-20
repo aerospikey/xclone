add83823220d1ff471b2acad0ed36bc61f426f25
aerospikey <111414109+aerospikey@users.noreply.github.com>
2024-10-20 11:51:41 +0700

    เมนูขวาไปก๊อปมา

--- src/app/(others)/layout.js
+++ src/app/(others)/layout.js
@@ -1,6 +1,8 @@
 import localFont from "next/font/local";
 import ".././globals.css";
 import Home from "./page";
+import LeftsideBar from "../components/LeftsideBar";
+import RightsideBar from "../components/RightsideBar";
 
 const geistSans = localFont({
   src: ".././fonts/GeistVF.woff",
@@ -22,9 +24,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+<div className="flex justify-between max-w-6xl mx-auto">
+  <div className="hidden sm:inline border-r h-screen sticky top-0">
+    <LeftsideBar/>
+  </div>
+<div className="w-2xl flex-1"> {children}</div>
+<div className="lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
+  <RightsideBar/>
+</div>
+</div>
       </body>
     </html>
   );
